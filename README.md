95cdc6a9ca1ecbe6d5bd7b395d813fcfde2dc005
TakkuMatssu <takku.mattsu@gmail.com>
2014-11-04 02:20:33 +0900

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ homebrew-refreshXcode
 =====================
 
 Clean Xcode Cache Script.
+
+
+Install and Use
+=================
+
+```bash
+# Install
+brew tap takkumattsu/homebrew-refreshXcode
+brew install refreshXcode
+# Use
+refreshXcode
+```
---
+++ refreshXcode.rb
@@ -0,0 +1,40 @@
+require "formula"
+
+# This is a non-functional example formula to showcase all features and
+# therefore, it's overly complex and dupes stuff just to comment on it.
+# You may want to use `brew create` to start your own new formula!
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+
+## Naming -- Every Homebrew formula is a class of the type `Formula`.
+# Ruby classes have to start Upper case and dashes are not allowed.
+# So we transform: `example-formula.rb` into `ExampleFormula`. Further,
+# Homebrew does enforce that the name of the file and the class correspond.
+# Check with `brew search` that the name is free.
+
+class Refreshxcode < Formula
+  homepage "http://github.com/takkumattsu/homebrew-refreshXcode.git" # used by `brew home example-formula`.
+
+  # The url of the archive. Prefer https (security and proxy issues):
+  url "https://gist.githubusercontent.com/TakkuMattsu/9242157/raw/700c32e42110aa0ff8217c74a4b039f8fb2cfaa8/refreshXcode.sh"
+
+  # For integrity and security, we verify the hash (`openssl dgst -sha1 <FILE>`)
+  # You may also use sha256 if the software uses sha256 on their homepage.
+  # Leave it empty at first and `brew install` will tell you the expected.
+  sha256 "9dd66db58ec9c9c060f189086bf5a71fd5540df8d49dea3fa74e3242232f5468"
+  version "1.0"
+
+  def install
+    system "mv /Library/Caches/Homebrew/refreshxcode-1.0.sh refreshXcode"
+    bin.install "refreshXcode"
+  end
+
+end
+
+__END__
+# Room for a patch after the `__END__`
+# Read about how to get a patch in here:
+#    https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+# In short, `brew install --interactive --git <formula>` and make your edits.
+# Then `git diff >> path/to/your/formula.rb`
+# Note, that HOMEBREW_PREFIX will be replaced in the path before it is
+# applied. A patch can consit of several hunks.
